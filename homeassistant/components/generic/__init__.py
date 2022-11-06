671e4116c0bfd89f109c17a76f550c73b839ad60
Goldcupidcraft <99417196+Goldcupidcraft@users.noreply.github.com>
2022-11-06 11:35:13 +0100

    Update __init__.py

--- homeassistant/components/generic/__init__.py
+++ homeassistant/components/generic/__init__.py
@@ -1,3 +1,35 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@Goldcupidcraft 
+home-assistant
+/
+core
+Public
+Code
+Issues
+1.6k
+Pull requests
+488
+Actions
+Projects
+2
+Security
+Insights
+core/homeassistant/components/generic/__init__.py /
+@davet2001
+davet2001 Remove invalid unique id from generic camera (#70568)
+…
+Latest commit d00d823 on 28 Apr
+ History
+ 4 contributors
+@bdraco@davet2001@Swamp-Ig@slovdahl
+ 48 lines (32 sloc)  1.62 KB
+
 """The generic component."""
 from __future__ import annotations
 
@@ -36,7 +68,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up generic IP camera from a config entry."""
 
     await _async_migrate_unique_ids(hass, entry)
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     entry.async_on_unload(entry.add_update_listener(_async_update_listener))
     return True
@@ -46,3 +78,18 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
 
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Release 2022.6.7 · home-assistant/corecore/__init__.py at 2022.6.7 · home-assistant/core
